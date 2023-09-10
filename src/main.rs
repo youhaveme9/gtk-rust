ef8e378d7a8bae355bad8ebab04d7354fa2fa416
Roshan Kumar <roshaen09@gmail.com>
2023-09-10 10:30:37 +0530

    added button

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use gtk::prelude::*;
-use gtk::{glib, Application, ApplicationWindow};
+use gtk::{glib, Application, ApplicationWindow, Button};
 
 const APP_ID: &str = "org.gtk_rs.HelloWorld1";
 
@@ -17,10 +17,27 @@ fn main() -> glib::ExitCode {
 
 fn build_ui(app: &Application) {
 
+    // crteated a button with label and margin on all sides
+    let button = Button::builder()
+        .label("Press Me!")
+        .margin_top(20)
+        .margin_bottom(20)
+        .margin_start(20)
+        .margin_end(20)
+        .build();
+
+    // added an event listner to the button
+    button.connect_clicked(|button| {
+        button.set_label("Hello World");  // changed the label of the button when it is clicked
+    });
+
+    
+
     // creating a blank window and setting the title
     let window = ApplicationWindow::builder()
         .application(app)
         .title("My First GTK App")
+        .child(&button)
         .build();
 
     // present the window to the screen
